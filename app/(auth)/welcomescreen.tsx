71c7931d8dde2a524d0cf10149e76d5e99369251
vishalBelwal <vishalbelwal52@gmail.com>
2025-05-27 23:44:54 +0530

    style: Adjust button width and margin in WelcomeScreen

--- app/(auth)/welcomescreen.tsx
+++ app/(auth)/welcomescreen.tsx
@@ -50,7 +50,7 @@ const WelcomeScreen = () => {
         ))}
       </Swiper>
 
-      <CustomButton title={isLastSlide ? "Get Started" : "Next"} className="w-11/12 mt-10" onPress={() => isLastSlide ? router.replace("/(auth)/sign-up"): swiperRef.current?.scrollBy(1)} />
+      <CustomButton title={isLastSlide ? "Get Started" : "Next"} className="w-9/12 mt-10 mb-10" onPress={() => isLastSlide ? router.replace("/(auth)/sign-up"): swiperRef.current?.scrollBy(1)} />
     </SafeAreaView>
   );
 };
