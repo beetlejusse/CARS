be163cca99569e6055a71a1680659ff567739dd3
vishalBelwal <vishalbelwal52@gmail.com>
2025-06-12 11:07:16 +0530

    sign-in fixed working properly now

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,40 +1,42 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { Alert, Image, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
 import { ScrollView } from "react-native";
 import { images } from "@/constants";
 import InputField from "@/components/InputField";
 import { icons } from "@/constants";
 import CustomButton from "@/components/CustomButton";
-import { Link, useRouter } from "expo-router";
+import { Link, router } from "expo-router";
 import OAuthComponent from "@/components/OAuthComponent";
-import { useSignIn } from '@clerk/clerk-expo'
+import { useSignIn } from "@clerk/clerk-expo";
 
 const SignIn = () => {
-  const { signIn, setActive, isLoaded } = useSignIn()
-  const router = useRouter()
+  const { signIn, setActive, isLoaded } = useSignIn();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-
-  const onSigninPress = async () => {
+  
+  const onSignInPress = useCallback(async () => {
     if (!isLoaded) return;
 
     try {
       const signInAttempt = await signIn.create({
         identifier: formData.email,
         password: formData.password,
-      })
-      if (signInAttempt.status === 'complete') {
-        await setActive({ session: signInAttempt.createdSessionId })
-        router.replace('/')
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(root)/(tabs)/home");
       } else {
-        console.error(JSON.stringify(signInAttempt, null, 2))
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
       }
-    } catch (err) {
-      console.error(JSON.stringify(err, null, 2))
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
     }
-  }
+  }, [isLoaded, formData, signIn, setActive, router]);
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -50,6 +52,7 @@ const SignIn = () => {
             label="Email"
             placeholder="email@domain.com"
             icon={icons.email}
+            textContentType="emailAddress"
             value={formData.email}
             onChangeText={(value) => setFormData({ ...formData, email: value })}
           />
@@ -59,6 +62,7 @@ const SignIn = () => {
             placeholder="Enter your password"
             icon={icons.lock}
             secureTextEntry={true}
+            textContentType="password"
             value={formData.password}
             onChangeText={(value) =>
               setFormData({ ...formData, password: value })
@@ -68,7 +72,7 @@ const SignIn = () => {
           <CustomButton
             title="Sign In"
             className="mt-6 shadow-lg shadow-black"
-            onPress={onSigninPress}
+            onPress={onSignInPress}
           />
 
           <OAuthComponent />
